7943339dafae6d220e20273ec6fd4c381e805f21
Eduardo Perin <du.perin@gmail.com>
2012-10-14 18:11:23 -0400

    Hello World

---
+++ HelloWorld/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Eduardo Perin on 10/14/12.
+//  Copyright (c) 2012 Eduardo Perin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        
+        NSString *variable = @"world";
+        NSLog(@"Hello, World!");
+        NSLog(@"Hello %@", variable);
+        
+        
+    }
+    return 0;
+}
+
